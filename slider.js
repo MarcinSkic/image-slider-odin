23fb334e00dc8e08a53dac0e0b86bf79c1405fc8
Marcin Skic <marcinskicoficjal@gmail.com>
2023-04-01 00:17:40 +0200

    Add direct jump buttons

--- slider.js
+++ slider.js
@@ -1,4 +1,5 @@
 const frame = document.querySelector(".slider-frame");
+const buttonsContainer = document.querySelector(".slider-buttons");
 //TODO: Take it from frame width
 const width = 200;
 let imageIndex = 0;
@@ -6,6 +7,7 @@ const imagesCount = frame.children.length;
 
 const getRight = function getRight(event) {
     const newIndex = imageIndex + 1 >= imagesCount ? 0 : imageIndex + 1;
+    console.log({ imageIndex, newIndex });
     setIndex(newIndex);
     event.preventDefault();
 };
@@ -16,14 +18,33 @@ const getLeft = function getLeft(event) {
     event.preventDefault();
 };
 
+const onDirectButton = function onDirectButton(event) {
+    setIndex(Number(event.currentTarget.dataset.index));
+    event.preventDefault();
+};
+
 //This function in the future should be single access point to any operation on slider, buttons just increment/decrement index through this
 const setIndex = function setIndex(value) {
+    const bigJump =
+        Math.abs(value - imageIndex) > 1 &&
+        Math.abs(value - imageIndex) != imagesCount - 1;
     imageIndex = value;
 
     [...frame.children].forEach((child, index) => {
         let difference = imageIndex - index;
-        difference =
-            Math.abs(difference) > imagesCount / 2 ? -difference : difference;
+        if (Math.abs(difference) > Math.floor(imagesCount / 2)) {
+            difference =
+                difference < 0
+                    ? imagesCount + difference
+                    : -imagesCount + difference;
+        }
+
+        if (Math.abs(difference) === Math.floor(imagesCount / 2) || bigJump) {
+            child.classList.add("not-animated");
+        } else {
+            child.classList.remove("not-animated");
+        }
+
         const targetWidth = difference * width;
         child.style.right = `${targetWidth}px`;
         child.dataset.difference = `${difference}`;
@@ -32,8 +53,18 @@ const setIndex = function setIndex(value) {
     console.log(`currentIndex: ${imageIndex}`);
 };
 
+const generateButtons = function generateButtons() {
+    [...frame.children].forEach((child, index) => {
+        const btn = document.createElement("button");
+        btn.dataset.index = `${index}`;
+        btn.addEventListener("click", onDirectButton);
+        buttonsContainer.append(btn);
+    });
+};
+
 const getRightBtn = document.querySelector(".get-right");
 const getLeftBtn = document.querySelector(".get-left");
 getRightBtn.addEventListener("click", getRight);
 getLeftBtn.addEventListener("click", getLeft);
+generateButtons();
 setIndex(0);
